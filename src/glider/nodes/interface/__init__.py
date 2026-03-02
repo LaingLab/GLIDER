5f7ac464824141716ed93c64bc34c9c9be1c009e
Garrett Bradham <bradhamgarrett@gmail.com>
2026-03-02 11:20:50 -0600

    Formatting issues

--- src/glider/nodes/interface/__init__.py
+++ src/glider/nodes/interface/__init__.py
@@ -5,15 +5,15 @@ These nodes expose controls to the Runner UI and allow
 users to interact with the experiment during execution.
 """
 
+from glider.nodes.interface.audio_nodes import (
+    AudioPlaybackNode,
+    register_audio_nodes,
+)
 from glider.nodes.interface.display_nodes import (
     ChartNode,
     GaugeNode,
     LabelNode,
     LEDIndicatorNode,
-)
-from glider.nodes.interface.audio_nodes import (
-    AudioPlaybackNode,
-    register_audio_nodes,
 )
 from glider.nodes.interface.input_nodes import (
     ButtonNode,
