0df6bc66c82157d6658be8f7fb833aa837743af8
gbradham <bradhamgarrett@gmail.com>
2026-06-03 15:33:13 -0500

    camera enumeration fix

--- src/glider/vision/camera_manager.py
+++ src/glider/vision/camera_manager.py
@@ -1078,7 +1078,7 @@ class CameraManager:
                                 )
                                 found_indices.add(i)
                                 cap.release()
-                                break  # Found with this backend, move to next index
+                                continue  # Found; advance to the next index
 
                             cap.release()
                         except Exception as e:
